cfa25900be1469e8e335d7c8834e1531f21f4562
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2026-03-02 10:08:39 +0530

    Update updater.js

--- updater.js
+++ updater.js
@@ -1,1 +1,35 @@
-// update profile commits here
+const { Octokit } = require("@octokit/rest");
+const fs = require("fs");
+
+const octokit = new Octokit({ auth: process.env.GHT });
+
+async function run() {
+    try {
+        const { data: user } = await octokit.users.getByUsername({
+            username: 'Spinachboul'
+        });
+
+        let template = fs.readFileSync("./template.svg", "utf8");
+
+        const stats = {
+            "{{USER}}": user.login,
+            "{{REPOS}}": user.public_repos.toString().padStart(2, '0'), // Adds a leading zero for style
+            "{{FOLLOWERS}}": user.followers.toString().padStart(2, '0'),
+            "{{GISTS}}": user.public_gists.toString().padStart(2, '0'),
+            "{{DATE}}": new Date().toLocaleDateString()
+        };
+
+        Object.keys(stats).forEach(key => {
+            template = template.replaceAll(key, stats[key]);
+        });
+
+        fs.writeFileSync("./template.svg", template);
+        console.log("Terminal stats generated!");
+
+    } catch (error) {
+        console.error("Error:", error);
+        process.exit(1);
+    }
+}
+
+run();
